8de456c00f1f613a03233fe0036a2595a630b1bc
rafsan3503 <rafsanchowdhuryrudro@gmail.com>
2022-11-24 00:26:28 +0600

    context create

--- src/AuthProvider/UserContext.js
+++ src/AuthProvider/UserContext.js
@@ -1,8 +1,67 @@
-import React from "react";
-import { getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  TwitterAuthProvider,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+// Create Context
+export const AuthContext = createContext();
+
+// get auth from firebase
+const auth = getAuth(app);
+
+// google and twitter provider
+const googleProvider = new GoogleAuthProvider();
+const twitterProvider = new TwitterAuthProvider();
 
 const UserContext = ({ children }) => {
-  return <div></div>;
+  // user
+  const [user, setUser] = useState("");
+  // loader
+  const [loading, setLoading] = useState(true);
+
+  // user inspection
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe;
+  }, []);
+
+  // create user
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // login user
+  const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // google logIn
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // twitter login
+  const twitterLogin = () => {
+    return signInWithPopup(auth, twitterProvider);
+  };
+  // value wrapper
+  const authInfo = { user, loading };
+  return (
+    <div>
+      <AuthContext.Provider>{children}</AuthContext.Provider>
+    </div>
+  );
 };
 
 export default UserContext;
